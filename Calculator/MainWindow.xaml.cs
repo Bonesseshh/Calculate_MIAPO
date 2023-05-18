71432a957fc8bc3ea4f3bc672a9bd24f61062eb6
Your Name <you@example.com>
2023-05-18 20:34:13 +0700

    End work

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -27,31 +27,12 @@ namespace Calculator
             
             foreach(UIElement el in MainRoot.Children)
             {
-                if(el is Button)
-                {
-                    ((Button) el).Click += Button_Click;
-                }
+                
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            string str = (string)((Button)e.OriginalSource).Content;
-
-            if (str == "AC")
-            {
-                textLabel.Text = "";
-            }
-            else if(str == "=")
-            {
-                string value = new DataTable().Compute(textLabel.Text, null).ToString();
-                textLabel.Text = value;
-            }
-            else
-            {
-                textLabel.Text += str;
-            }
+        
             
-        }
+        
     }
 }
