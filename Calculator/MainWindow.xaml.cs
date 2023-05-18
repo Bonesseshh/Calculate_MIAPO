88311a70512a2c5feb6267200ff4d9d85338e0a3
Your Name <you@example.com>
2023-05-18 20:54:06 +0700

    Finish work

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -25,9 +25,33 @@ namespace Calculator
         {
             InitializeComponent();
             
-            
+            foreach(UIElement el in MainRoot.Children)
+            {
+                if(el is Button)
+                {
+                    ((Button) el).Click += Button_Click;
+                }
+            }
         }
 
-       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC")
+            {
+                textLabel.Text = "";
+            }
+            else if(str == "=")
+            {
+                string value = new DataTable().Compute(textLabel.Text, null).ToString();
+                textLabel.Text = value;
+            }
+            else
+            {
+                textLabel.Text += str;
+            }
+            
+        }
     }
 }
